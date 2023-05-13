e1c9d9a6fe6b324707a9ce08be65780062995575
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-13 17:50:10 -0300

    requisito 3 foi

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,6 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
+  if (employeeName) {
+    return data.employees
+      .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  }
+
+  return {};
   // seu código aqui
 };
 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,7 +3,6 @@ const data = require('../data/zoo_data');
 const getSpeciesByIds = (...ids) => {
   // seu código aqui
   if (ids) {
-    // console.log('🍌');
     return data.species.filter((item) => ids.includes(item.id));
   }
 
