0104698472cfaf5235a2639454532497f4b5d1c3
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-13 17:00:56 -0300

    requisito 2 foi

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
+const getAnimalsOlderThan = (animal, idade) => {
+  const arrayFilter = data.species.find((item) => item.name.toLowerCase() === animal.toLowerCase());
+  const checkAge = (element) => element.age >= idade;
+  return arrayFilter.residents.every(checkAge);
   // seu código aqui
 };
 
