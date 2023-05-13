3b3a42a90f45eb1fa1a5f7b1a77b86bf79d0cc73
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-13 16:14:44 -0300

    requisito 1 foi

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  
   // seu código aqui
 };
 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
   // seu código aqui
+  if (ids) {
+    // console.log('🍌');
+    return data.species.filter((item) => ids.includes(item.id));
+  }
+
+  return [];
 };
 
 module.exports = getSpeciesByIds;
