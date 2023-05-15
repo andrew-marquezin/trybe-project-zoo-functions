d48d35fd3acd457fc7c2a6df4407f6e36d5161e6
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-15 03:18:43 -0300

    requisito 4 foi

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,25 @@ const data = require('../data/zoo_data');
 
 const isManager = (id) => {
   // seu código aqui
+  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+  const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+  const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+  const managers = [stephanieId, olaId, burlId];
+  return managers.includes(id);
 };
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managed = data.employees.filter((employee) => employee.managers.includes(managerId));
+  const managedArray = [];
+  managed.forEach((element) => {
+    const { firstName, lastName } = element;
+    managedArray.push([firstName, lastName].join(' '));
+  });
+  return managedArray;
 };
 
 module.exports = { isManager, getRelatedEmployees };
