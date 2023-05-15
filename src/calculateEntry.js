a8a0c019f5acc32dbafe188b4876f648dbb0d4f8
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-15 16:31:01 -0300

    requisito 8 foi

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 const countEntrants = (entrants) => {
   // seu código aqui
+  const count = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      count.child += 1;
+    } else if (person.age >= 50) {
+      count.senior += 1;
+    } else {
+      count.adult += 1;
+    }
+  });
+  return count;
 };
 
 const calculateEntry = (entrants) => {
+  if (!entrants || !entrants.length) return 0;
+
+  const ages = countEntrants(entrants);
+  const child = ages.child * prices.child;
+  const adult = ages.adult * prices.adult;
+  const senior = ages.senior * prices.senior;
+
+  return child + adult + senior;
   // seu código aqui
 };
 
