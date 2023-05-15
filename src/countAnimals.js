b10764d94a43083015481ca580de62f61d0453d6
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-15 04:29:16 -0300

    requisito 5 foi

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,19 @@ const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
   // seu código aqui
+  if (!animal) {
+    const retorno = {};
+    data.species.forEach((bicho) => {
+      retorno[bicho.name] = bicho.residents.length;
+    });
+    return retorno;
+  }
+  const filtrado = data.species.find((item) => animal.species === item.name);
+  if (Object.keys(animal).length > 1) {
+    return filtrado.residents.filter((residente) => residente.sex === animal.sex).length;
+  }
+  return filtrado.residents.length;
 };
+// console.log(countAnimals());
 
 module.exports = countAnimals;
