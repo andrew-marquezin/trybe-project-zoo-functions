141e4573b8e36ddd38d73af814b98ec6c46cc73e
andrew-marquezin <andrewmmarinho@gmail.com>
2023-05-17 17:08:03 -0300

    requisito 10 foi

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
   // seu código aqui
+  const animalId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstAnimal = data.species.find((animal) => animal.id === animalId);
+
+  const oldestAnimal = firstAnimal.residents.reduce((lastAnimal, currentAnimal) =>
+    (currentAnimal.age > lastAnimal.age ? currentAnimal : lastAnimal));
+  return Object.values(oldestAnimal);
 };
 
 module.exports = getOldestFromFirstSpecies;
